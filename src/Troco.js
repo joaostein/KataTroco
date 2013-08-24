bbb15b36c6fc8a41a2f6f390c20a3a6b240e7a56
Joao Stein <joaosteing@gmail.com>
2013-08-23 23:35:00 -0300

    Removed spaces inside money array and moved it directly to the logic that creates the result string

--- src/Troco.js
+++ src/Troco.js
@@ -1,14 +1,14 @@
 var Operator = function() {
 
-    this.money = [[100,  '100 '],
-                  [50,   '50 '],
-                  [10,   '10 '],
-                  [5,    '5 '],
-                  [1,    '1 '],
-                  [0.5,  '0.50 '],
-                  [0.1,  '0.10 '],
-                  [0.05, '0.05 '],
-                  [0.01, '0.01 ']];
+    this.money = [[100,  '100'],
+                  [50,   '50'],
+                  [10,   '10'],
+                  [5,    '5'],
+                  [1,    '1'],
+                  [0.5,  '0.50'],
+                  [0.1,  '0.10'],
+                  [0.05, '0.05'],
+                  [0.01, '0.01']];
 
     this.change = function(totalPrice, payment) {
 
@@ -19,7 +19,7 @@ var Operator = function() {
 
         for (var i = 0, len = this.money.length; i < len; i++) {
             while(change >= this.money[i][0]) {
-                result += this.money[i][1];
+                result += this.money[i][1] + ' ';
                 change -= this.money[i][0];
 
                 change = Math.round(change * 100) / 100;
