bcecfed678958e6774fd7e30aa5ee3eefb785238
Joao Stein <joaosteing@gmail.com>
2013-08-23 21:42:44 -0300

    Edited money array to not be global

--- src/Troco.js
+++ src/Troco.js
@@ -1,14 +1,14 @@
 var Operator = function() {
 
-    money = [[100, '100 '],
-             [50, '50 '],
-             [10, '10 '],
-             [5, '5 '],
-             [1, '1 '],
-             [0.5, '0.50 '],
-             [0.1, '0.10 '],
-             [0.05, '0.05 '],
-             [0.01, '0.01 ']];
+    this.money = [[100,  '100 '],
+                  [50,   '50 '],
+                  [10,   '10 '],
+                  [5,    '5 '],
+                  [1,    '1 '],
+                  [0.5,  '0.50 '],
+                  [0.1,  '0.10 '],
+                  [0.05, '0.05 '],
+                  [0.01, '0.01 ']];
 
     this.change = function(totalPrice, payment) {
 
@@ -17,10 +17,10 @@ var Operator = function() {
 
         change = Math.round(change * 100) / 100;
 
-        for (var i = 0, len = money.length; i < len; i++) {
-            while(change >= money[i][0]) {
-                result += money[i][1];
-                change -= money[i][0];
+        for (var i = 0, len = this.money.length; i < len; i++) {
+            while(change >= this.money[i][0]) {
+                result += this.money[i][1];
+                change -= this.money[i][0];
 
                 change = Math.round(change * 100) / 100;
             }
